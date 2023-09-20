4a371be971f12db00832cd1e13442a4b47f83b3b
Jacob Rangel <jacobrangel0628@gmail.com>
2023-09-19 21:47:35 -0500

    Update main.cpp

--- Programming Assignments/P19/main.cpp
+++ Programming Assignments/P19/main.cpp
@@ -22,20 +22,99 @@ int calcPoints(vector <int> subArray1, vector <int> subArray2){
 }
 
 void merge(vector <int> &robert, vector <int> & rachel, int left, int mid, int right){
+  int leftSize = mid - left + 1;
+  int rightSize = right - mid;
+
+  vector <int> robertLeft;
+  vector <int> robertRight;
+  vector <int> rachelLeft;
+  vector <int> rachelRight;
+
+  // copy the subarrays over to the vectors
+  for (int i = 0; i < leftSize; i++){
+    robertLeft[i] = robert[left + i];
+    rachelLeft[i] = rachel[left + i];
+  }
+
+  for (int j = 0; j < rightSize; j++){
+    robertRight[j] = robert[mid + 1 + j];
+    rachelRight[j] = rachel[mid + 1 + j];
+  }
+
+  // helper here
+  int i = 0;
+  int j = 0;
   
+
+  // create indexes for saving the indexes of the main array and subarrays
+  int robert_i = 0;
+  int robert_j = 0;
+  int robert_k = left;
+
+  int rachel_i = 0;
+  int rachel_j = 0;
+  int rachel_k = left;
+
+  while (robert_i < leftSize && robert_j < rightSize){
+    if (robertLeft[robert_i] <= robertRight[robert_j]){
+      robert[robert_k] = robertLeft[robert_i];
+      robert_i++;
+    }
+    else{
+      robert[robert_k] = robertRight[robert_j];
+      robert_j++;
+    }
+    robert_k++;
+  }
+
+  while (rachel_i < leftSize && rachel_j < rightSize){
+    if (rachelLeft[rachel_i] <= rachelRight[rachel_j]){
+      rachel[rachel_k] = rachelLeft[rachel_i];
+      rachel_i++;
+    }
+    else{
+      rachel[rachel_k] = rachelRight[rachel_j];
+      rachel_j++;
+    }
+    rachel_k++;
+  }
+
+  // if there are remaining elements leftover in the Left and Right subarrays for robert and rachel
+  while (robert_i < leftSize){
+    robert[robert_k] = robertLeft[robert_i];
+    robert_i++;
+    robert_k++;
+  }
+
+  while (robert_j < rightSize){
+    robert[robert_k] = robertRight[robert_j];
+    robert_j++;
+    robert_k++;
+  }
+
+  while (rachel_i < leftSize){
+    rachel[rachel_k] = rachelLeft[rachel_i];
+    rachel_i++;
+    rachel_k++;
+  }
+
+  while (rachel_j < rightSize){
+    rachel[rachel_k] = rachelRight[rachel_j];
+    rachel_j++;
+    rachel_k++;
+  }
 }
 
 void mergeSort(vector <int> robert, vector <int> rachel, int left, int right){
-  if (left >= right)
-    return;
+  if (left < right){
+    int mid = left + (right - left) / 2;
 
-  int mid = left + (right - left) / 2;
-
-  // lefts
-  mergeSort(robert, rachel, left, mid);
-  // rights
-  mergeSort(robert, rachel, mid + 1, right);
-  merge(robert, rachel, left, mid, right);
+    // lefts
+    mergeSort(robert, rachel, left, mid);
+    // rights
+    mergeSort(robert, rachel, mid + 1, right);
+    merge(robert, rachel, left, mid, right);
+  } 
 }
 
 int main(){
@@ -55,4 +134,5 @@ int main(){
       rachel.push_back(num);
     
     mergeSort(robert, rachel, 0, rachel.size()-1);
+    cout << robertPoints << " " << rachelPoints;
 }
