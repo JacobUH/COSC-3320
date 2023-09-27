36553c349560e597ed47baf243222684f726402c
Jacob Rangel <jacobrangel0628@gmail.com>
2023-09-27 18:09:39 -0500

    updated P17 and and P19

--- Programming Assignments/P17/main.cpp
+++ Programming Assignments/P17/main.cpp
@@ -3,10 +3,39 @@
 #include <vector>
 using namespace std;
 
-int countRotations(vector<int> &nums){
-
+int countRotations(vector<int> &nums, int left, int mid){
+    return 0;
 }
-int binarySearch(vector<int> &nums, int target){
+
+int binarySearch(vector<int> &nums, int left, int mid, int right, int target){
+    while (left <= right){
+        mid = (left + right) / 2;
+        if (target == nums[mid])
+            return mid;
+
+        // left subarray
+        if (nums[left] <= nums[mid]){
+            // search right subarray conditions
+            if (target > nums[mid] || target < nums[left]){
+                left = mid+1;
+            }
+            // search left conditions
+            else{
+                right = mid-1;
+            }
+        }
+        // right subarray
+        else{
+            // search left subarray conditions
+            if (target < nums[mid] || target > nums[right]){
+                right = mid-1;
+            }
+            // search right conditons
+            else{
+                left = mid+1;
+            }
+        }
+    }
     return -1;
 }
 
@@ -24,8 +53,10 @@ int main(){
         arr.push_back(num);
     cin >> value;
 
-    int k = countRotations(arr);
-    int target = binarySearch(arr, value);
+    int mid = arr.size()/2;
+
+    int k = countRotations(arr, 0, arr.size()-1);
+    int target = binarySearch(arr, 0, mid, arr.size()-1, value);
     cout << k << endl << target;
 
     return 0;
