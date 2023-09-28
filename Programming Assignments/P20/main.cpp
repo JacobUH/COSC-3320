8e730cd7a41cec44e558b3a372f8dfcc1a30e46b
Jacob R <jacobrangel0628@gmail.com>
2023-09-28 14:01:57 -0500

    completed P20

--- Programming Assignments/P20/main.cpp
+++ Programming Assignments/P20/main.cpp
@@ -1,28 +1,45 @@
+// 7b257f24-3fa6-4cf2-812d-ed45ae48b9a2
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 // backtracking
-vector<int> steppingNumbers(int low, int high){
-    vector <int> permutations;
+vector<int> steppingNumbers(int low, int high) {
+    vector<int> steppingNums;
 
-    // base case
-    while (low < high){
-        int prev = -1;
-        while (low){
-            int cur = low % 10;
+    for (int num = low; num <= high; ++num) {
+        int prevDigit = -1;
+        int temp = num;
+        bool isStepping = true;
+
+        while (temp > 0) {
+            int curDigit = temp % 10;
+
+            if (prevDigit != -1 && abs(prevDigit - curDigit) != 1) {
+                isStepping = false;
+                break;
+            }
+
+            prevDigit = curDigit;
+            temp /= 10;
+        }
+
+        if (isStepping) {
+            steppingNums.push_back(num);
         }
     }
-    return permutations;
+
+    return steppingNums;
 }
 
-int main(){
-    int low;
-    int high;
+int main() {
+    int low, high;
     cin >> low >> high;
-    vector <int> combinations = steppingNumbers(low, high);
+    vector<int> combinations = steppingNumbers(low, high);
 
-    for (int i: combinations)
+    for (int i : combinations)
         cout << i << " ";
+
     return 0;
 }
